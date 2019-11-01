5f1959fc8b01ea8e68b0467522e73b739ff4dd81
Yannis Torres <yann.torres@gmail.com>
2019-11-01 15:08:43 +0100

    feat(init): initial commit with project configuration

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const env = require("./config/env");
+const publicConfig = require("./config/public.runtime");
+const serverConfig = require("./config/server.runtime");
+
+const nextConfig = {
+  webpack: (config, options) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: "empty"
+    };
+    return config;
+  },
+  compress: false
+};
+
+module.exports = () => {
+  /* see https://github.com/zeit/next.js#build-time-configuration  */
+  nextConfig.env = env;
+  /* see https://github.com/zeit/next.js#runtime-configuration  */
+  nextConfig.publicRuntimeConfig = publicConfig;
+  nextConfig.serverRuntimeConfig = serverConfig;
+
+  const withCSS = require("@zeit/next-css");
+  return withCSS(nextConfig);
+};
